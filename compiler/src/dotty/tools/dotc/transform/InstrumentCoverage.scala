a02c366c403c25bc02aeae67ec00150b50f1362c
Anatolii Kmetiuk <anatoliikmt@proton.me>
2026-03-17 17:04:26 +0900

    Fix transform call in InstrumentCoverage to account for unsafeNulls import

--- compiler/src/dotty/tools/dotc/transform/InstrumentCoverage.scala
+++ compiler/src/dotty/tools/dotc/transform/InstrumentCoverage.scala
@@ -400,7 +400,7 @@ class InstrumentCoverage extends MacroTransform with IdentityDenotTransformer:
           case tree: PackageDef =>
             if isFileIncluded(tree.srcPos.sourcePos.source) && isClassIncluded(tree.symbol) then
               // only transform the statements of the package
-              cpy.PackageDef(tree)(tree.pid, transform(tree.stats))
+              cpy.PackageDef(tree)(tree.pid, transformStats(tree.stats, tree.symbol))
             else
               tree
 
